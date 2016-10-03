8fa2d3879a5dd2affa1d4eaeab55476e91de4057
ShaunApps <shaun.apps@gmail.com>
2016-10-03 16:59:45 -0400

    some styling and formating stuff for views

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,6 @@
 class JobsController < ApplicationController
   before_action :set_job, only: [:show, :edit, :update, :destroy]
-  skip_before_action CASClient::Frameworks::Rails::Filter, only: [:new, :create, :show]
+  skip_before_action CASClient::Frameworks::Rails::Filter, only: [:new, :create, :show, :index]
   # GET /jobs
   # GET /jobs.json
   def index
