070bd43ed8465ff1810fb1e4cb1e51769c99d6b9
ShaunApps <shaun.apps@gmail.com>
2016-10-09 15:22:19 -0400

    small tweak

--- config/environment.rb
+++ config/environment.rb
@@ -12,6 +12,6 @@ require 'casclient/frameworks/rails/filter'
 CASClient::Frameworks::Rails::Filter.configure(
    :cas_base_url => "http://login.wyncode.co",
    :extra_attributes_session_key => :cas_extra_attributes,
-  #  :service_url => #"http://localhost:3000/" 
+  #  :service_url => "http://localhost:3000/"
    :service_url => "https://boiling-depths-97825.herokuapp.com/"
 )
